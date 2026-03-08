257986557cdd097bf597c711eb2ba733116783a1
Albin Binu <albinbinu.me@gmail.com>
2026-03-08 21:12:39 +0530

    return

--- functions.py
+++ functions.py
@@ -2,9 +2,22 @@
 function is A BLOCK OF CODE only executed when we called
 """
 
-def hello(fname,lname,age):
-    print("hello " + fname +" "+ lname)
-    print("you're "+ str(age))
+# def hello(fname,lname,age):
+#     print("hello " + fname +" "+ lname)
+#     print("you're "+ str(age))
+#
+#
+# hello("Albin","b",21)
+
+"""
+
+return - send a py value back to the caller. 
+
+"""
 
 
-hello("Albin","b",21)
+def mult(x,y):
+    return x * y
+
+sum = mult(2,3)
+print(sum)
