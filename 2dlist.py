a43df23159606c8f477c4e7f31e9343f1302c51f
Albin Binu <albinbinu.me@gmail.com>
2026-03-09 11:50:55 +0530

    added 2d list

---
+++ 2dlist.py
@@ -0,0 +1,14 @@
+"""
+2d list - list of list
+"""
+
+
+drinks = ["coffee","soda","tea"]
+dinner = ["pizza","hamburger","hotdog"]
+dessert = ["cake","icecream"]
+
+food = [drinks,dinner,dessert]
+
+# print(food)
+
+# print(food[0][0]) coffee
