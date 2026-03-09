3677dc57ab7b7741e3f64c704384eec6d2d881bc
Albin Binu <albinbinu.me@gmail.com>
2026-03-09 15:35:57 +0530

    added else block

--- exception_handling.py
+++ exception_handling.py
@@ -8,7 +8,7 @@ try:
     firstNumber = int(input("enter first number: "))
     secondNumber = int(input("enter second number: "))
     answer = firstNumber / secondNumber
-    print(answer)
+
 except ZeroDivisionError as e:
     print(e)
     print("dividing by ZERO isn't allowed")
@@ -20,6 +20,7 @@ except KeyboardInterrupt as e:
     print("keyboard is interrupted")
 except Exception as e:
     print(e,"occurred")
-
+else:
+    print(answer)
 
 
