709dfb771ba576bf0a4b041dbbde302d9f6f5635
Albin Binu <albinbinu.me@gmail.com>
2026-03-09 11:32:08 +0530

    created list, print items & items by index, printed using loops (for,while)

---
+++ lists.py
@@ -0,0 +1,17 @@
+"""
+
+used to store multiple items in a single varible
+
+"""
+
+food = ["sandwich","hotdog","burger","donut","salad"]
+# print(food)
+# print(food[0])
+# food[0] = "biriyani"
+# print(food)
+# for i in food:
+#     print(i)
+i = 0
+while i < len(food):
+    print(food[i])
+    i += 1
