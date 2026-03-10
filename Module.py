29d7f48e5399e8463a648660b6181f8595bbcafe
Albin Binu <albinbinu.me@gmail.com>
2026-03-10 16:13:01 +0530

    created py modules and import and anaylsed

---
+++ Module.py
@@ -0,0 +1,35 @@
+"""
+modules are file contain py code
+using modules
+check message.py
+"""
+
+# method 1
+
+# import message
+#
+# message.sum(1,2,3)
+# message.message()
+
+# method 2
+
+# import message as msg
+#
+# msg.sum(1,2,3)
+# msg.message()
+
+# method 3
+
+# from message import sum,message
+#
+# message()
+# sum(1,2,3)
+
+# method 4
+
+# from message import *
+#
+# message()
+# sum(1,2,3)
+
+help('modules')
---
+++ message.py
@@ -0,0 +1,10 @@
+
+def message():
+    print("hello world")
+
+
+def sum(*args):
+    sum = 0
+    for i in args:
+        sum += i
+    print(sum)
